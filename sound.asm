ceaa5a1d63878f82427dd0422d6bbb87e298db8b
Evan C. Wright <evancwright@yahoo.com>
2018-05-16 10:58:24 -0500

    added static

--- sound.asm
+++ sound.asm
@@ -97,6 +97,32 @@ ytimes6
 	puls d
 	rts
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;static
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+play_static	
+	pshs d,x,y
+ 	ldy snd_data+3  ; save sound data
+	pshs y 
+	ldy snd_data+5
+	pshs y 
+	ldy static_start   ; random bytes
+	sty snd_data+3
+	leay 25,y          ; stop addr is 25 bytes past
+	sty snd_data+5
+	lda #$30  ; high pitch
+	sta delay_value
+	pshs y
+	ldy #$1  ; short length
+	jsr sound_play
+	puls y
+	sty static_start   ; update static start
+	puls y
+	sty snd_data+5
+	puls y
+	sty snd_data+3
+	puls d,x,y
+	rts
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;PLAYS A TONE WHOSE INFO IS STORED IN THE 
 ;SND DATA STRUCTURE
@@ -247,161 +273,4 @@ freq .db 0x01
 sound_length .db 0x00
 tone_length .dw 0x00ff
 delay_value .db 128
-
-win_music
-	.db 9
-	.db 0
-	.db $70  ; C
-	.dw 60	
-	.db $52
-	.dw 70
-	.db $3f
-	.dw 90
-	.db $70
-	.dw 60
-	.db $52
-	.dw 70
-	.db $3F
-	.dw 90
-	.db $70
-	.dw 60
-	.db $52
-	.dw 70
-	.db $3F
-	.dw 90
-	
-wumpus_music
-	.db 16  ; num notes
-	.db 0  ; curNote
-	.db $ef ; f
-	.dw 75
-	.db 0  ; pause
-	.dw 0
-	.db $ef ;f
-	.dw 50
-	.db 0 ; pause
-	.dw 0
-	.db $ef ; f
-	.dw 25
-	.db 0 ; pause
-	.dw 0
-	.db $ef  ; f
-	.dw 75
-	.db $cd  ; g#
-	.dw 50
-	.db $d5  ; g
-	.dw 25
-	.db 0 ; pause
-	.dw 0
-	.db $d5  ; g
-	.dw 50
-	.db $ef  ;f
-	.dw 25
-	.db 0  ; pause
-	.dw 0
-	.db $ef ; f
-	.dw 50
-	.db $ff ;e 
-	.dw 25
-	.db $ef ;f 
-	.dw 75
-
-fall_music
-	.db 17  ; num sounds
-	.db 0 ; cursound
-	.db 10
-	.dw 80
-	.db 10
-	.dw 80
-	.db 11
-	.dw 80
-	.db 11
-	.dw 70
-	.db 12
-	.dw 50
-	.db 13
-	.dw 45
-	.db 14
-	.dw 50
-	.db 15
-	.dw 50
-	.db 16
-	.dw 50
-	.db 17
-	.dw 45
-	.db 18
-	.dw 40
-	.db 19
-	.dw 40
-	.db 20
-	.dw 40
-	.db 21
-	.dw 40
-	.db 22
-	.dw 40
-	.db 23
-	.dw 40
-	.db 24
-	.dw 40
-	.db 25
-	.dw 40
-	.db 26
-	.dw 40
-	.db 27
-	.dw 40
-	
-intro_music
-	.db 26
-	.db 0
-	.db $86  ; A
-	.dw 75  
-	.db $77  ; B
-	.dw 75  
-	.db $70  ; C
-	.dw 75  
-	.db $64  ; D
-	.dw 75  	
-	.db $57  ; E
-	.dw 75  		
-	.db $70  ; C
-	.dw 75  
-	.db $57  ; E
-	.dw 125  		
-	.db $5E  ; e flat
-	.dw 75
-	.db $77  ; B
-	.dw 75  
-	.db $5e  ; e flat
-	.dw 125
-	.db $64  ; D
-	.dw 75 
-	.db $7E  ; B flat
-	.dw 75 
-	.db $64  ; D
-	.dw 125 
-	.db $86  ; A
-	.dw 75  
-	.db $77  ; B
-	.dw 75  
-	.db $70  ; C
-	.dw 75  
-	.db $64  ; D
-	.dw 75  	
-	.db $57  ; E
-	.dw 75  		
-	.db $70  ; C
-	.dw 75
-	.db $86  ; A
-	.dw 75 
-	.db $40  ; A high
-	.dw 120  
-	.db $48  ; G high
-	.dw 100  		
-	.db $57  ; E
-	.dw 75  		
-	.db $70  ; C
-	.dw 70	
-	.db $57  ; E
-	.dw 75  
-	.db $48  ; G high
-	.dw 150  	
+ 	
