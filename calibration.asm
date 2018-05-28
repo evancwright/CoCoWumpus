0767d0a9f996373b065687ebb2a22cd7c9d93bd5
Evan C. Wright <evancwright@yahoo.com>
2018-05-27 22:38:46 -0500

    calibration screen improvement

--- calibration.asm
+++ calibration.asm
@@ -10,12 +10,49 @@
 	
 	lda #WHITE_FILL
 	jsr cls
+	; draw prompt
+	lda #12 ;width
+	pshu a
+	lda #22 ;height
+	pshu a
+	lda #10 ;x
+	ldb #50 ;y
+	ldy #press_reset
+	jsr draw_sprite
+	;draw colored squares
+;	lda #4
+;	ldb #4
+;	jsr set_draw_offset
+;	ldx #color_calibration
+;	jsr draw_tile
+	lda #3
+	ldb #4
+	jsr set_draw_offset
+	ldx #sprite_wumpus_left
+	jsr draw_tile
 	lda #4
 	ldb #4
 	jsr set_draw_offset
-	ldx #color_calibration
+	ldx #sprite_wumpus_right
 	jsr draw_tile
-	
+	;press any key
+	lda #12
+	pshu a
+	lda #6
+	pshu a
+	lda #10
+	ldb #140
+	ldy #press_a_key
+	jsr draw_sprite
+	;if correct
+	lda #9
+	pshu a
+	lda #6
+	pshu a
+	lda #12
+	ldb #150
+	ldy #if_correct
+	jsr draw_sprite
 @lp2
 	jsr KBSCAN
 	cmpa #0
