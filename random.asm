52680619cc83fb3ae6ca1ab1a96e9f5dc15083f2
evancwright <evancwright@yahoo.com>
2019-05-26 15:51:58 +0100

    fixd code overwriting itself

--- random.asm
+++ random.asm
@@ -1,12 +1,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;NEXT_RAND
-;THIS SUBROUTINE USES A LINEAR SHIFT
-;TO IMPLEMENT RANDOM NUMBER GENERATION
-;THE TAPS ARE BITS 0 AND 3
+;next_rand
+;this subroutine uses a linear shift
+;to implement random number generation
+;the taps are bits 0 and 3
 ;
-;THE SEED (CUR_RAND) SHOULD BE LOADED BEFORE 
-;THIS ROUTINE IS CALLED.  JUST DON'T LOAD
-;IT WITH ALL ZEROS TO START 0
+;the seed (cur_rand) should be loaded before 
+;this routine is called.  just don't load
+;it with all zeros to start 0
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -21,66 +21,53 @@ randmod
 	rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;LSR RANDOM NUMBER GENERATOR
-;ALL REGISTERS ARE PRESERVED
+;lsr random number generator
+;all registers are preserved
 ;number is returned on the user stack
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 rand
-	PSHS D,x
-	LDB CUR_RAND+1 ; GET RIGHT BYTE
-	ANDB LEFT_TAP_MASK ; MASK RAND TO GET LEFT TAP	
-	STB LEFT_TAP			;SAVE IT
-	LDB CUR_RAND+1		;RELOAD
-	ANDB RIGHT_TAP_MASK	; MASK RAND TO GET RIGHT TAP
-	;RIGHT TAP IS THE LSB (SO WE DON'T NEED TO SHIFT)
-	STB RIGHT_TAP
-	LSL RIGHT_TAP	;LEFT JUSTIFY THE RIGHT TAP 
-	LSL RIGHT_TAP	;SO THE TWO TAPS CAN BE 
-	LSL RIGHT_TAP	;XOR'D AND EASILY
-	LSL RIGHT_TAP	;MASKED BACK ONTO THE LEFT
-	LSL RIGHT_TAP	;BYTE OF THE RANDOM NUMBER
-	LSL RIGHT_TAP
-	LSL RIGHT_TAP
-	;NOW WE HAVE BOTH TAPS, XOR THEM
-	LDA LEFT_TAP
-	EORA RIGHT_TAP	
-	STA XOR_RSLT
-	;NOW SHIFT
-	LDD CUR_RAND	;LOAD LEFT BYTE
-	LSRA ; PUTS BIT 0 INTO CARRY BIT 
-	STA CUR_RAND ; STORE NEW MSB
-	BCC @nc
-	LDB	CUR_RAND+1	;THERE WAS A CARRY
-	LSRB
-	ORB ONE_IN_MSB ;  MASK A 1 ONTO THE LEFT MOST BIT
-	BRA @ds	 ;  DONE SHIFTING
+	pshs d,x
+	ldb cur_rand+1 ; get right byte
+	andb left_tap_mask ; mask rand to get left tap	
+	stb left_tap			;save it
+	ldb cur_rand+1		;reload
+	andb right_tap_mask	; mask rand to get right tap
+	;right tap is the lsb (so we don't need to shift)
+	stb right_tap
+	lsl right_tap	;left justify the right tap 
+	lsl right_tap	;so the two taps can be 
+	lsl right_tap	;xor'd and easily
+	lsl right_tap	;masked back onto the left
+	lsl right_tap	;byte of the random number
+	lsl right_tap
+	lsl right_tap
+	;now we have both taps, xor them
+	lda left_tap
+	eora right_tap	
+	sta xor_rslt
+	;now shift
+	ldd cur_rand	;load left byte
+	lsra ; puts bit 0 into carry bit 
+	sta cur_rand ; store new msb
+	bcc @nc
+	ldb	cur_rand+1	;there was a carry
+	lsrb
+	orb one_in_msb ;  mask a 1 onto the left most bit
+	bra @ds	 ;  done shifting
 @nc	
-	LDB	CUR_RAND+1 ; JUST SHIFT, NO 1 ON LEFT
-	LSRB
-@ds STB CUR_RAND+1 	; STORE RIGHT HALF OF NEW RAND
-	;NOW MASK THE XOR_RSLT ONTO THE MSB
-	LDA CUR_RAND
-	ORA XOR_RSLT	; MASK A '1' ONTO THE MSB
-	STA CUR_RAND
-	;SUBTRACT 1 SINCE THE WORKING RAND CAN'T CONTAIN 0
-	LDD CUR_RAND	; RELOAD 2 BYTE VALUE
-	SUBD #1		; DEC
-	STD RAND			
-        ldx CUR_RAND 
+	ldb	cur_rand+1 ; just shift, no 1 on left
+	lsrb
+@ds stb cur_rand+1 	; store right half of new rand
+	;now mask the xor_rslt onto the msb
+	lda cur_rand
+	ora xor_rslt	; mask a '1' onto the msb
+	sta cur_rand
+	;subtract 1 since the working rand can't contain 0
+	ldd cur_rand	; reload 2 byte value
+	subd #1		; dec
+	std cur_rand			
+        ldx cur_rand 
 	pshu x
-	PULS x,D
-	RTS	
+	puls x,d
+	rts	
 
-;DATA FOR RANDOM NUMBER GENERATION
-CUR_RAND			.DW 0x0F0F
-RAND				.DW 0x0000
-ONE_IN_MSB		.DB 0x80
-LEFT_TAP_MASK 	.DB 0x80 ; 1000 0000
-RIGHT_TAP_MASK 	.DB 0x01 ; 0000 0001
-LEFT_TAP  		.DB 0x00
-RIGHT_TAP  		.DB 0x00
-XOR_RSLT			.BYTE 0x00
-OUTPUT
-	.DW 0X0000
-	.DW 0X0000
-	.DW 0X0000
